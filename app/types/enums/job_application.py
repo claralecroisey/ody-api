d2f31df2732d6e3af981f40f5e96bf7a5ad18815
Clara Lecroisey <clalecroisey@gmail.com>
2023-11-22 20:40:17 +0100

    feat: add offer_received value to the JobApplicationStatus enum

--- app/types/enums/job_application.py
+++ app/types/enums/job_application.py
@@ -4,9 +4,10 @@ from enum import StrEnum
 class JobApplicationStatus(StrEnum):
     saved = "saved"
     applied = "applied"
-    rejected = "rejected"
     in_process = "in_process"
+    rejected = "rejected"
     offer_rejected = "offer_rejected"
     offer_accepted = "offer_accepted"
+    offer_received = "offer_received"
     closed = "closed"
     withdrawn = "withdrawn"
